5a315dd3695fee2f3c4f179a278d6782f3be7f45
VICTOR VALENCIA <victor.valencia@gmail.com>
2014-10-25 00:08:55 -0500

    Orientacion

--- www/js/index.js
+++ www/js/index.js
@@ -50,21 +50,3 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
-window.addEventListener("orientationchange", function() {
-   alert(window.orientation);
-});
-
-function checkConnection() {
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
-}
