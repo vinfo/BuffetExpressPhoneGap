ab9e49993aac50f137b7bd65b042d13cab642dae
VICTOR VALENCIA <victor.valencia@gmail.com>
2014-10-28 16:41:24 -0500

    Angular

---
+++ www/js/directives.js
@@ -0,0 +1,43 @@
+﻿angularRoutingApp.directive('addressBasedGoogleMap', function () {
+    return {
+        restrict: "A",
+        template: "<div id='addressMap'></div>",
+        scope: {
+            address: "=",
+            zoom: "="
+        },
+        controller: function ($scope) {
+            var geocoder;
+            var latlng;
+            var map;
+            var marker;
+            var initialize = function () {
+                geocoder = new google.maps.Geocoder();
+                latlng = new google.maps.LatLng(-34.397, 150.644);
+                var mapOptions = {
+                    zoom: $scope.zoom,
+                    center: latlng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById('addressMap'), mapOptions);
+            };
+            var markAdressToMap = function () {
+                geocoder.geocode({ 'address': $scope.address }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        map.setCenter(results[0].geometry.location);
+                        marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location
+                        });
+                    }
+                });
+            };
+            $scope.$watch("address", function () {
+                if ($scope.address != undefined) {
+                    markAdressToMap();
+                }
+            });
+            initialize();
+        },
+    };
+});
---
+++ www/js/main.js
@@ -0,0 +1,52 @@
+// Creación del módulo
+var angularRoutingApp = angular.module('angularRoutingApp', ['ngRoute']);
+
+// Configuración de las rutas
+angularRoutingApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl	: 'templates/slider.html',
+			controller 	: 'mainController'
+		})
+		.when('/menu', {
+			templateUrl : 'templates/menu.html',
+			controller 	: 'menuController'
+		})
+		.when('/compras', {
+			templateUrl : 'templates/compras.html',
+			controller 	: 'comprasController'
+		})
+		.when('/mapa', {
+			templateUrl : 'templates/mapa.html',
+			controller 	: 'mapaController'
+		})		
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+angularRoutingApp.controller('menuController', function($scope) {
+	$scope.message = 'Esta es la página "Acerca de"';
+});
+
+angularRoutingApp.controller('comprasController', function($scope) {
+	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
+});
+
+angularRoutingApp.controller('mapaController', function($scope) {
+	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
+});
+
+angularRoutingApp.directive('wrapOwlcarousel', function () {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            var options = scope.$eval($(element).attr('data-options'));
+            var owl =$(element).owlCarousel(options);
+            options.goTo(3);
+        }
+    };
+});
+angularRoutingApp.controller("mainController", ["$scope", function mainController($scope) {    
+    $scope.Adress = "American University,4400 Massachusetts Ave NW,Washington, DC 20016";
+}]);
