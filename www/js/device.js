adb422d2e8baa9c5292ec14d025180d93e620fb5
VICTOR VALENCIA <victor.valencia@gmail.com>
2014-12-18 10:54:51 -0500

    GPS

--- www/js/device.js
+++ www/js/device.js
@@ -28,17 +28,17 @@
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener("offline", checkConnection, false);
+        //document.addEventListener("offline", checkConnection, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
         screen.lockOrientation('portrait-primary');
-        navigator.splashscreen.show();
-        window.cache.clear( alertDismissed, alertDismissed );
+        //navigator.splashscreen.show();
+        //window.cache.clear( alertDismissed, alertDismissed );
         checkConnection;
         GetGeoLocation;
     },
@@ -78,14 +78,33 @@ function checkConnection() {
 function  GetGeoLocation () {
   var options =  { maximumAge: 3000, timeout: 3000, enableHighAccuracy: true };
   navigator.geolocation.getCurrentPosition(ShowPosition, ShowError, options);
+  //var dist=getDistance({lat:position.coords.latitude,lng:position.coords.longitude},{lat:6.250756,lng:-75.568008});
 }
 
 function ShowPosition(position) {
     alert("Latitude: " + position.coords.latitude +
           "Longitude: " + position.coords.longitude);
+    localStorage.setItem("position",position.coords.latitude+"|"+position.coords.longitude);
 }
 function ShowError(error) {
    alert("Errorcode: "    + error.code    +
          "Errormessage: "+ error.message );
 }
 
+/* Calacular distancia */
+var rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+//var dist=getDistance({lat:lat,lng:lng},{lat:6.250756,lng:-75.568008}); 
+var getDistance = function(p1, p2) {  
+  var R = 6378137; // Earth’s mean radius in meter
+  var dLat = rad(p2.lat - p1.lat);
+  var dLong = rad(p2.lng - p1.lng);
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d; // returns the distance in meter
+};
