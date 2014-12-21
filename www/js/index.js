04a557e7fc8df35f352f62ae0d462bbdeb57bbb8
VICTOR VALENCIA <victor.valencia@gmail.com>
2014-12-21 13:21:26 -0500

    sadasf

--- www/js/index.js
+++ www/js/index.js
@@ -2,11 +2,14 @@ app.initialize();
 startApp();
 
 function startApp() {
+    alert("Start");
     if (navigator.geolocation) {
+        alert("Geolocation");
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
         alert("Geolocation is not supported by this browser.");
     }
+    alert("End");
 }
 function showPosition(position) {
     alert("Latitude: " + position.coords.latitude +
