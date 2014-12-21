edfc061ca7f6131bd5d40b31b4446c7a962cdc86
VICTOR VALENCIA <victor.valencia@gmail.com>
2014-12-21 14:05:55 -0500

    sdafaf

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,48 @@
-//app.initialize();
+app.initialize();
 startApp();
 
-function startApp() {
-    alert("Start");
-    var lat1="";
-    var lng1="";
-    alert("zones1");  
-    var zones= JSON.parse(getZone());
-    alert(JSON.stringify(zones));
-    alert("zones2");  
-    localStorage.setItem("zona",JSON.stringify({id:1,code:'cam001'}));    
-    if (navigator.geolocation) {
-        alert("Geolocation");
-        redirect();
-    } else {
-        alert("Geolocalización no soportada en dispositivo");
-        redirect();
-    }
-    alert("End");
+function startApp() {  
+  alert("startApp");
+  localStorage.domain = "http://buffetexpress.co/REST/";  
+  localStorage.dimension = $(window).width();
+  alert(localStorage.dimension);  
+  var lat1="";
+  var lng1="";    
+  var zones= JSON.parse(getZone());  
+  localStorage.setItem("zona",JSON.stringify({id:1,code:'cam001'}));
+  if (navigator.geolocation) {
+      alert("Init GEO");
+      navigator.geolocation.getCurrentPosition(
+              function(position) {
+                lat1= position.coords.latitude;
+                lng1= position.coords.longitude;     
+                localStorage.setItem("position",JSON.stringify({lat:lat1,lng:lng1})); 
+                var datos=[];
+                var distancias=[];               
+                for(var i=0;i<zones.length;i++){
+                  var coord= zones[i].coordinates.split(',');
+                  if(coord[1]){
+                    var dist= getDistance({lat:lat1,lng:lng1},{lat:coord[0],lng:coord[1]});
+                    var data= {id:zones[i].id,code:zones[i].code};
+                    distancias.push(dist);
+                    datos.push(data);
+                  }
+                }
+                distancias.sort();
+                if(distancias.length>0){
+                  localStorage.setItem("zona",JSON.stringify(datos[0])); 
+                }      
+                redirect();
+              },
+              function(error) {
+                  redirect();
+              },
+              {timeout: 30000, enableHighAccuracy: true, maximumAge: 75000}
+      );
+  }else{
+    alert("Geolocalización no soportada en dispositivo!");
+    redirect();
+  }  
 }
 
 function redirect(){
@@ -27,20 +52,19 @@ function redirect(){
          }, 1200);   
 }
 
-function getZone(){   
+function getZone(){
     var data= "token=false";
     var res="";
     $.ajax({
        type: 'GET',
-       url: 'http://buffetexpress.co/REST/api/v1/getZones/',
+       url: localStorage.domain+'api/v1/getZones/',
        crossDomain: true,
        data: data,
        dataType: 'json',
        async: false, 
-       success: function(msg) {         
-         res= JSON.stringify(msg);  
-         alert(res); 
-       }     
+       success: function(msg) {
+         res= JSON.stringify(msg);   
+       }
      });
      return res;
 }
