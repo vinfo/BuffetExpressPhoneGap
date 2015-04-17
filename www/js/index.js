d4373325bd99cac868d1f0841c9e6babfa00a00d
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-04-17 09:27:03 -0500

    Timer

--- www/js/index.js
+++ www/js/index.js
@@ -9,6 +9,7 @@ function startApp() {
 	localStorage.removeItem("valor_recomendado");
 	localStorage.removeItem("zonas");
 	localStorage.removeItem("MsgZone");	
+	localStorage.removeItem("timer");
 	
     window.setTimeout(function() {
 		window.location.href = 'load.html';
