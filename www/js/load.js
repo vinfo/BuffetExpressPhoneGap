da7dbfdce20ac812204905466074c6998e77d997
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-05-14 09:34:33 -0500

    Test GEo

--- www/js/load.js
+++ www/js/load.js
@@ -1,4 +1,23 @@
-startApp();
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    startApp();    
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+    startApp();
+}
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 function startApp() {
       if(localStorage.GeoError=="true"){
