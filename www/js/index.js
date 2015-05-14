d42d7113690165e0289defcff563eac1988e4334
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-05-14 15:00:25 -0500

    Noti

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,11 @@
 app.initialize();
+navigator.notification.alert(
+            'Prueba',
+            alertDismissed, 
+            'Alerta',
+            'Aceptar'
+        );
+function alertDismissed() {
+    // do something
+}        
 //window.location.href = 'load.html';
