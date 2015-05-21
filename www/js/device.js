2fbf80f983739794f0bf48002e33a5aa99b638f0
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-05-20 19:17:07 -0500

    Location

--- www/js/device.js
+++ www/js/device.js
@@ -39,16 +39,10 @@
         //alert("Iniciando app..");
         //app.receivedEvent('deviceready');        
         //navigator.splashscreen.show();
-        var options = {
-          enableHighAccuracy: true,
-          timeout: 10000,
-          maximumAge: 75000
-        };
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
         checkConnection();
         getDeviceProperty();
         navigator.splashscreen.hide(); 
-        screen.lockOrientation('portrait-primary');        
+        screen.lockOrientation('portrait-primary');
     }
 };
 
@@ -88,35 +82,9 @@ function getDeviceProperty()
          /* alert("Device OS: " + deviceOS); 
           alert("Device OS Version: " + deviceOSVersion);
           */
-      }
-      function getNameURLWeb(){
-         var sPath = window.location.pathname;
-         var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
-         return sPage;
-     }
-// onSuccess Geolocation
-    //
-    function onSuccess(position) {
-        localStorage.removeItem("position");
-        localStorage.removeItem("quadrant");
-        localStorage.removeItem("valor_buffet");
-        localStorage.removeItem("valor_domicilio");
-        localStorage.removeItem("valor_recomendado");
-        localStorage.removeItem("zonas");
-        localStorage.removeItem("MsgZone"); 
-        localStorage.removeItem("timer");        
-        lat1= position.coords.latitude;
-        lng1= position.coords.longitude;   
-        var pos= {lat:lat1,lng:lng1};
-        localStorage.setItem("position",JSON.stringify(pos));
-        window.location.href = 'load.html';
-    }
-
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        navigator.notification.alert('Para utilizar esta APP debes permitir tu ubicación\nVe a Ajustes, selecciona la aplicación Buffet Express y permite el acceso a tu ubicación', null, 'Alerta', 'Aceptar');
-        var pos= {lat:6.250909,lng:-75.565608};
-        localStorage.setItem("position",JSON.stringify(pos));
-        window.location.href = 'load.html';
-    }
+}
+function getNameURLWeb(){
+ var sPath = window.location.pathname;
+ var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+ return sPage;
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,32 @@
 app.initialize();
-//window.location.href = 'load.html';
+var options = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+  maximumAge: 75000
+};
+navigator.geolocation.getCurrentPosition(onSuccess, onError, options); 
+
+// onSuccess Geolocation    //
+function onSuccess(position) {
+  localStorage.removeItem("position");
+  localStorage.removeItem("quadrant");
+  localStorage.removeItem("valor_buffet");
+  localStorage.removeItem("valor_domicilio");
+  localStorage.removeItem("valor_recomendado");
+  localStorage.removeItem("zonas");
+  localStorage.removeItem("MsgZone"); 
+  localStorage.removeItem("timer");        
+  lat1= position.coords.latitude;
+  lng1= position.coords.longitude;   
+  var pos= {lat:lat1,lng:lng1};
+  localStorage.setItem("position",JSON.stringify(pos));
+  window.location.href = 'load.html';
+}
+
+// onError Callback receives a PositionError object    //
+function onError(error) {
+  navigator.notification.alert('Para utilizar esta APP debes permitir tu ubicación\nVe a Ajustes, selecciona la aplicación Buffet Express y permite el acceso a tu ubicación', null, 'Alerta', 'Aceptar');
+  var pos= {lat:6.250909,lng:-75.565608};
+  localStorage.setItem("position",JSON.stringify(pos));
+  window.location.href = 'load.html';
+}
