8cbc39e5fd992c348239cc2e1a3bda0ea64b1aca
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-09-10 14:23:10 -0500

    Analitics

--- www/js/device.js
+++ www/js/device.js
@@ -44,12 +44,12 @@
         getDeviceProperty();
         navigator.splashscreen.hide(); 
         screen.lockOrientation('portrait-primary');
-        // window.analytics.startTrackerWithId('UA-62739338-1');
-        // window.analytics.trackView('/index');
-        // window.analytics.trackView('/internal');
-        // window.analytics.trackView('/login');
-        // window.analytics.trackView('/templates/guia');
-        // window.analytics.trackView('/templates/contactenos');
+        window.analytics.startTrackerWithId('UA-62739338-1');
+        window.analytics.trackView('/index');
+        window.analytics.trackView('/internal');
+        window.analytics.trackView('/login');
+        window.analytics.trackView('/templates/guia');
+        window.analytics.trackView('/templates/contactenos');
         // window.analytics.debugMode();//Debug   
     }
 };
