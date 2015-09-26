8b4cb848f3c078684d0fca635607b8e41fb3dad3
VICTOR VALENCIA <victor.valencia@gmail.com>
2015-09-26 11:38:32 -0500

    Sector coords

--- www/js/sector.js
+++ www/js/sector.js
@@ -5,7 +5,7 @@ if (window.jQuery) {
 		for (var i=0; i < zonas.length; i++){
 			if(zonas[i].closez==1){
 				$('#zonas').append($('<option>', { 
-					value: zonas[i].code+"|"+zonas[i].id,
+					value: zonas[i].code+"|"+zonas[i].id+"|"+zonas[i].coordinates,
 					text : zonas[i].name
 				}));
 			}
@@ -22,13 +22,13 @@ if (window.jQuery) {
 			localStorage.setItem("plato",1);
 			localStorage.setItem("tipo_pago","efectivo");  
 			localStorage.setItem("quadrant","");    	
-			localStorage.setItem("zona",JSON.stringify({id:2,code:data[0]}));      	
+			localStorage.setItem("zona",JSON.stringify({id:2,code:data[0],coords:data[2]}));      	
 		}else{		
 			localStorage.setItem("plato",1);
 			localStorage.setItem("tipo_pago","efectivo");      	
 			localStorage.setItem("quadrant","d");
 			localStorage.setItem("MsgZone",1);
-			localStorage.setItem("zona",JSON.stringify({id:data[1],code:data[0]}));
+			localStorage.setItem("zona",JSON.stringify({id:data[1],code:data[0],coords:data[2]}));
 		}
 		var timer= new Date().getTime();
 		localStorage.setItem("timer",timer); 
