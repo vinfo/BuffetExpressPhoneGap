0a48ad3cf5dfa3ed998b3c266fe2ef28a511d779
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-06-22 13:39:01 -0500

    cac

--- www/js/internal.js
+++ www/js/internal.js
@@ -120,7 +120,7 @@ function shareBono(){
   var descuento= $("#descuento_bono").html();
   var imagen= $("#img_qr").attr("src");
   var codigo= $("#codigo").html();
-  window.plugins.socialsharing.share('Hola,\r\nDescarga la aplicación Buffet Express y almuerza por $ '+precio+', podes armar tu plato como quieras en el primer restaurante online tipo buffet.\r\nEs muy fácil entra a la app y en el menú encontraras la opción para escanear el código QR que te llego con este mensaje o ingresa el siguiente código: '+codigo+',\r\nYa que tienes tu bono por $ '+descuento+' has tu orden y paga menos.\r\nEs que poder elegir; también es rico!.', null,imagen, 'http://buffetexpress.com.co/'); 
+  window.plugins.socialsharing.share('Hola, como estas?\r\nOhi! descárgate esta aplicación Buffet Express y almorza hoy por $ '+precio+', podes armar tu plato como queras…\r\nEs muy fácil entra a la app y en el menú dale click a comparte y gana, escanea el código QR que te llego con este mensaje o ingresa el siguiente código: '+codigo+', que te dara un bono de $ '+descuento,null,imagen, 'http://buffetexpress.co/'); 
 }
 function scanear(){
    cordova.plugins.barcodeScanner.scan(
